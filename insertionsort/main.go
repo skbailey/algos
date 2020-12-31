011dcba93869a2fed7026e15c2de9f9d5ab2b0e6
Sherard Bailey <sherard@uniteus.com>
2020-12-31 02:17:31 -0500

    Add insertionsort

---
+++ insertionsort/main.go
@@ -0,0 +1,13 @@
+package insertionsort
+
+// Sort an array using the insertion sort algorithm
+func Sort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			}
+		}
+	}
+	return arr
+}
--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"algos/insertionsort"
 	"algos/mergesort"
+	"algos/quicksort"
 	"fmt"
 )
 
 func main() {
 	unsortedArray := []int{5, 7, 2, -3, 9, -5}
 	fmt.Println("Unsorted Array", unsortedArray)
-	// sortedArray := quicksort.Sort(unsortedArray, 0, len(unsortedArray)-1)
-	sortedArray := mergesort.Sort(unsortedArray)
-	fmt.Println("Sorted Array", sortedArray)
+
+	// Quick Sort
+	sortedArray := quicksort.Sort(unsortedArray, 0, len(unsortedArray)-1)
+	fmt.Println("quick sort complete", sortedArray)
+
+	// Merge Sort
+	unsortedArray = []int{5, 7, 2, -3, 9, -5}
+	sortedArray = mergesort.Sort(unsortedArray)
+	fmt.Println("merge sort complete", sortedArray)
+
+	// Insertion Sort
+	unsortedArray = []int{5, 7, 2, -3, 9, -5}
+	sortedArray = insertionsort.Sort(unsortedArray)
+	fmt.Println("insertion sort complete", sortedArray)
 }
